1d6f5898cf4c2c81593f66107a8d1a56dcb60cf0
branko96 <branko-96@hotmail.com>
2022-10-26 00:03:40 -0300

    RN Vector Icons and tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/screens/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        error: 'red',
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+        red: colors.red,
+        green: colors.green,
       },
     },
   },
